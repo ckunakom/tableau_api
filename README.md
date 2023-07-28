5b7e296acc5e16f5f19737f4ec7bd6f2a897d22c
ckunakom <ckunakom@gmail.com>
2023-07-28 19:16:05 -0400

    Added more function in all_user_none_permission.py and updated Rreadme

--- README.md
+++ README.md
@@ -21,14 +21,14 @@ Fill out `env.txt` with your API token and Tableau Server information and rename
 Here's where you can create a Personal Access Token: https://help.tableau.com/current/online/en-us/security_personal_access_tokens.htm  
 
 ## Download Workbooks for Git Revision
-Quick shout-out to this youtube video as it saves me the trouble of some readings🤫: https://youtu.be/swasTFYM_Gs
-
 One of the capabilities that Tableau Server does not offer is integration with Git. You can view the previous revisions with Tableau Server, but only up to certain number of revisions (I think it's up to 20? 25?). As someone who used to work in Audit and Monitoring department, that was heart-breaking. However, not all hope is lost since there is a an effective way of downloading the workbooks without using the UI, which involves lot of multiple mouse-clicking.
 
 Run the `download_workbook.py` and follow the prompt. It's interactive!
 - User will select some numbers corresponding to the project and the workbooks they need to download
 - Once the selection is complete, the workbooks will be downloaded and place in a directory which gets created with the same name of the workbook (if one doesn't already exist).
 - The files are ready to be added, committed and push to the repo. 
+
+Quick shout-out to this youtube video as it saves me the trouble of some readings🤫: https://youtu.be/swasTFYM_Gs
 
 ## Create Groups, Projects and Set Permission for each group for the Project
 This is particularly useful when you have different Tableau sites and/or servers that you need to configure the same group permission with the same project name. Save you a lot of mouse-clicking.
--- all_user_none_permission.py
+++ all_user_none_permission.py
@@ -1,6 +1,14 @@
 # Dependencies
 from query_groups import *
 
+##################################################################################
+'''When using Tableau Rest API to create projects, the 'All Users' 
+    group will be automatically configured to have both Write and 
+    Read permission allowed within the project as well as other capability
+    object (data source, workbooks, lens, etc.). Remove the permission 
+    to give 'All Users' a None permission configuration with these functions.'''
+##################################################################################
+
 def get_all_users_id():
     '''Get the group id of All User.'''
     group_list = get_group()
@@ -9,38 +17,79 @@ def get_all_users_id():
     return all_users_id
 
 # Default Capability for Project, Flow, Data Roles, Lens and Metrics
-perm_capability = ['Write', 'Read']
+    '''perm_capability = ['Write', 'Read']'''
 
 def project_remove_group_perm(project_id, group_id, perm_capability):
-    '''Remove previously configured Read & Write permission to a group for a project.
-        When using Tableau Rest API to create projects, the All Users group
-        will be automatically configured to have both Write and Read permission allowed.
-        Remove the permission to give All Users a None permission configuration with 
-        this function.'''
+    '''Remove previously configured Read & Write permission to a group for a project.'''
     for p in perm_capability:
         response = conn.delete_project_permission(
             project_id=project_id,
-            delete_permissions_object="groups",
+            delete_permissions_object='groups',
             delete_permissions_object_id=group_id,
             capability_name=p,
-            capability_mode="Allow",
+            capability_mode='Allow',
         )
         print(f'Removed {p} capabililty from group id {group_id} HTTP status: {response.status_code}')
 
 
-
 def other_remove_group_default_perm(project_id, group_id, perm_capability):
-    default_perm_obj = ["flows", "dataroles", "lenses", "metrics"]
+    '''Remove default configuration of Read & Write permission to a group for other
+        permission object (flow, data roles, lens, metrics) within a project.'''
+    
+    default_perm_obj = ['flows', 'dataroles', 'lenses', 'metrics']
     
     for p in perm_capability:
-    
         for d in default_perm_obj:
             response = conn.delete_default_permission(
                 project_id=project_id,
                 project_permissions_object=d,
-                delete_permissions_object="group",
+                delete_permissions_object='group',
                 delete_permissions_object_id=group_id,
                 capability_name=p,
-                capability_mode="Allow",
+                capability_mode='Allow',
             )
         print(f'Removed {p} capabililty for {d} from group id {group_id} HTTP status: {response.status_code}')
+
+def workbook_remove_group_default_perm(project_id, group_id):
+    '''Remove default configured permission of Workbooks to a group.'''
+    
+    workbook_perm = ['Filter', 'Read', 'ViewComments', 'AddComment', 'ExportData', 
+        'ExportImage', 'RunExplainData', 'ShareView', 'ViewUnderlyingData', 'Write',
+    ]
+
+    print('--------------------------------------------------------------')
+    print(f'Workbooks capabililty from group id {group_id} HTTP status:')
+
+    for w in workbook_perm:
+        response = conn.delete_default_permission(
+            project_id=project_id,
+            project_permissions_object='workbooks',
+            delete_permissions_object='group',
+            delete_permissions_object_id=group_id,
+            capability_name=w,
+            capability_mode='Allow',
+        )
+        print(f'Removed {w}: {response.status_code}')
+    
+    print('--------------------------------------------------------------')
+
+def datasource_remove_group_default_perm(project_id, group_id):
+    '''Remove default configured permission of Data source to a group.'''
+    
+    datasource_perm = ["Read", "Write", "Connect"]
+    
+    print('--------------------------------------------------------------')
+    print(f'Data Sources capabililty from group id {group_id} HTTP status:')
+    
+    for d in datasource_perm:
+        response = conn.delete_default_permission(
+            project_id=project_id,
+            project_permissions_object="datasources",
+            delete_permissions_object="group",
+            delete_permissions_object_id=group_id,
+            capability_name=d,
+            capability_mode="Allow",
+        )
+        print(f'Removed {d}: {response.status_code}')
+    
+    print('--------------------------------------------------------------')
